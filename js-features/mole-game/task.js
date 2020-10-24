8ef821676cfc4c3a87dfd29188c48803593b0c81
batyss <batsy8219@gmail.com>
2020-10-24 20:32:37 +0300

    task 1.1.3_upd

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,21 +1,33 @@
 getHole = index => document.getElementById(`hole${index}`);
 
+for (i = 1; i < 10; i++) {
+	getHole(i).onclick = shot;
+}
+
 let mole = 0;
 let miss = 0;
 
 function shot() {
 	if (this.className.includes('hole_has-mole')) {
-			mole += 1;
-		} else {
-			miss += 1;
-		};
+		mole += 1;
+		document.getElementById(`dead`).textContent = mole;
+		if (mole === 10) {
+			alert ('Победа!');
+			resetCounters();
+		}
+	} else {
+		miss += 1;
+		document.getElementById(`lost`).textContent = miss;
+		if (miss === 5) {
+			alert ('Вы проиграли(');
+ 			resetCounters();
+		}
+	}
 }
 
-for (i = 1; i < 10; i++) {
-	getHole(i).onclick = shot;
-}
-
-if (mole === 10 || miss === 5) {
+function resetCounters() {
 	mole = 0;
  	miss = 0;
+ 	document.getElementById(`dead`).textContent = mole;
+ 	document.getElementById(`lost`).textContent = miss;
 }
