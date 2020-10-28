04d48efa8134ad7b76430aade3918b433bb394b2
batyss <batsy8219@gmail.com>
2020-10-28 23:50:23 +0300

    task 1.2.1_upd

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,11 +1,11 @@
-document.getElementById('modal_main').className = 'modal_active';
+document.getElementById('modal_main').className = 'modal modal_active';
 
 for (let i = 0; i < 2; i++) {
 	document.getElementsByClassName('modal__close_times')[i].onclick = closeWindow;
 }
 
 const btnDanger = document.getElementsByClassName('show-success')[0];
-btnDanger.addEventListener('click', () => document.getElementById('modal_success').className = 'modal_active');
+btnDanger.addEventListener('click', () => document.getElementById('modal_success').className = 'modal modal_active');
 btnDanger.addEventListener('click', closeWindow);
 
 function closeWindow() {
