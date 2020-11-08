b020554ca8a7486735c56628a494805150156187
batyss <batsy8219@gmail.com>
2020-11-08 20:54:48 +0300

    task 1.2.2_upd

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -4,17 +4,20 @@ for (let i = 0; i < links.length; i++) {
 	links[i].onclick = pushMenu;
 }
 
-function pushMenu() {
+function pushMenu(event) {
 	if (this.nextElementSibling.className.includes('menu_sub')) {
-		if (document.querySelector('.menu_active')) { //.querySelector('.menu_sub .menu_active') не работает
-		let openedMenu = document.querySelectorAll('.menu_sub');
-		for (let i = 0; i < openedMenu.length; i++) {
-			openedMenu[i].classList.remove('menu_active');
-		}
+		if (this.nextElementSibling.classList.contains('menu_active')) {
+			this.nextElementSibling.classList.remove('menu_active');
+			event.preventDefault();
+			return;
+		} else if (document.querySelector('.menu_sub.menu_active')) {
+			//document.querySelector('.menu_sub.menu_active').classList.remove('menu_active'); // кажется, можно без цикла вовсе
+			let openedMenu = document.querySelectorAll('.menu_sub.menu_active');
+			for (let i = 0; i < openedMenu.length; i++) {
+				openedMenu[i].classList.remove('menu_active');
+			}
 		}	
+		event.preventDefault();
 		this.nextElementSibling.classList.add('menu_active');
-		this.onclick = this.classList.remove('menu_active');
-		return false;
-	}
+	}	
 }
-
