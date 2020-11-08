925c492b9be932209283641575a1cdde43491160
batyss <batsy8219@gmail.com>
2020-11-08 20:57:22 +0300

    task 1.2.3_upd

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,45 +1,54 @@
 const leftArrow = document.querySelector('.slider__arrow_prev');
 const rightArrow = document.querySelector('.slider__arrow_next');
-let q = document.querySelectorAll('.slider__item').length;
+const slides = document.querySelectorAll('.slider__item');
 const dots = document.getElementsByClassName('slider__dot');
+const Dots = Array.from(dots);
+let q = slides.length;
 let n = 0;
 
 leftArrow.onclick = pushPrev;
 rightArrow.onclick = pushNext;
 
+for (let i = 0; i < dots.length; i++) {
+	dots[i].onclick = pushDot;
+}
+
+getActive(n);
+
+function getActive() {
+	slides[n].classList.add('slider__item_active');
+	for (let i = 0; i < dots.length; i++) {
+		dots[i].classList.remove('slider__dot_active');
+	}
+	dots[n].classList.add('slider__dot_active');
+}
+
 function pushPrev() {
-	let currentSlide = document.querySelector('.slider__item_active');
-	currentSlide.classList.remove('slider__item_active');
+	slides[n].classList.remove('slider__item_active');
 	if (n === 0) {
 		n = q;
 	}
-	dots[n - 1].onclick(); // вот здесь непонятно
-	let previousSlide = document.getElementsByClassName('slider__item')[n -= 1];
-		previousSlide.classList.add('slider__item_active');
+	getActive(n -= 1);
 }
 
 function pushNext() {
-	let currentSlide = document.querySelector('.slider__item_active');
-	currentSlide.classList.remove('slider__item_active');
+	slides[n].classList.remove('slider__item_active');
 	if (n === q - 1) {
 		n = -1;
 	}
-	dots[n + 1].onclick(); // и здесь
-	let nextSlide = document.getElementsByClassName('slider__item')[n += 1];
-	nextSlide.classList.add('slider__item_active');
+	getActive(n += 1);
 }
 
-for (let i = 0; i < dots.length; i++) {
-	dots[i].onclick = pushDot;
-	//dots[i].addEventListener('click', pushDot);
-	//dots[i].addEventListener('click', changeSlide);
-}
-
-function pushDot() {
+function pushDot(event) {
 	for (let i = 0; i < dots.length; i++) {
-	dots[i].classList.remove('slider__dot_active');
+		dots[i].classList.remove('slider__dot_active');
 	}
-	this.classList.add('slider__dot_active');
+	event.currentTarget.classList.add('slider__dot_active');
+	slides[n].classList.remove('slider__item_active');
+	n = Dots.findIndex(isActiveDot);
+	slides[n].classList.add('slider__item_active');
 }
 
-//function changeSlide() {}
+function isActiveDot(element, index, Dots) {
+	return element.classList.contains('slider__dot_active');
+}
