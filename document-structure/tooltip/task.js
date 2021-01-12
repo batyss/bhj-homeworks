6e883b260bf001010049c5c95e2702edac71ef3a
batyss <batsy8219@gmail.com>
2021-01-13 00:54:57 +0300

    task 2.3.1_upd

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,16 +1,17 @@
+const toolTip = document.createElement('div');
+toolTip.classList.add('tooltip');
 const refs = document.querySelectorAll('.has-tooltip');
 for (item of refs) {
 	item.addEventListener('click', showTooltip);
-	item.insertAdjacentHTML('afterend', `<div class="tooltip">${item.title}</div>`)
 }
 
-function showTooltip(event) {
+function showTooltip() {
 	event.preventDefault();
-	const toolTips = document.querySelectorAll('.tooltip');
-	for (item of toolTips) {
-		item.classList.remove('tooltip_active');
-	}
-	let currTooltip = event.target.nextElementSibling;
-	currTooltip.classList.add('tooltip_active');
-	currTooltip.style = `top: ${event.target.getBoundingClientRect().top + 18}px; left: ${event.target.getBoundingClientRect().left}px`;
-}
+	toolTip.classList.toggle('tooltip_active');
+	if (document.querySelector('.tooltip') !== this.nextElementSibling) {
+		this.insertAdjacentElement('afterend', toolTip);
+		toolTip.textContent = `${this.title}`;
+		toolTip.style = `top: ${this.getBoundingClientRect().top + 18}px; left: ${this.getBoundingClientRect().left + 3}px`;
+		toolTip.classList.add('tooltip_active');
+	} 	
+}	
