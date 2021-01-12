3c8a29516dba590aa2983bbb0f22c94987e5bf7a
batyss <batsy8219@gmail.com>
2021-01-13 00:57:26 +0300

    task 2.3.2_upd

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,11 +1,14 @@
 const enterForm = document.getElementById('tasks__form');
-enterForm.addEventListener('submit', (evt) => evt.preventDefault());
 const tasksList = document.getElementById('tasks__list');
 const inputField = document.getElementById('task__input');
-inputField.addEventListener('keydown', addTask);
+const btn = document.getElementById('tasks__add');
+
+enterForm.addEventListener('submit', (evt) => evt.preventDefault());
+enterForm.addEventListener('keydown', addTask);
+enterForm.addEventListener('click', addTask);
 
 function addTask(event) {
-	if (inputField.value != '' && event.keyCode === 13) {
+	if (inputField.value.trim() != '' && (event.keyCode === 13 || event.target === btn)) {	
 		tasksList.insertAdjacentHTML('beforeend', 
        		`<div class="task">
 			<div class="task__title">${inputField.value}</div>
