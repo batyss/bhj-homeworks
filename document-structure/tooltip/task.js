f7a28dd8db4d6f124203eefec59e57a06395dfd8
batyss <batsy8219@gmail.com>
2021-01-13 23:57:12 +0300

    task 2.3.1_upd_upd

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -7,11 +7,12 @@ for (item of refs) {
 
 function showTooltip() {
 	event.preventDefault();
-	toolTip.classList.toggle('tooltip_active');
-	if (document.querySelector('.tooltip') !== this.nextElementSibling) {
-		this.insertAdjacentElement('afterend', toolTip);
-		toolTip.textContent = `${this.title}`;
-		toolTip.style = `top: ${this.getBoundingClientRect().top + 18}px; left: ${this.getBoundingClientRect().left + 3}px`;
-		toolTip.classList.add('tooltip_active');
-	} 	
-}	
+	if ((toolTip.textContent === this.title) && document.querySelector ('.tooltip_active')) {
+		toolTip.classList.remove('tooltip_active');
+		return;
+	}
+	this.insertAdjacentElement('afterend', toolTip);
+	toolTip.textContent = `${this.title}`;
+	toolTip.style = `top: ${this.getBoundingClientRect().top + 18}px; left: ${this.getBoundingClientRect().left + 3}px`;
+	toolTip.classList.add('tooltip_active');	
+}
